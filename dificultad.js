32a5e8248e7e35c6cff0e947fcc67faf985fa65c
AndreiCarillo <36821284+AndreiCarillo@users.noreply.github.com>
2021-01-31 10:12:25 -0500

    Add files via upload

---
+++ dificultad.js
@@ -0,0 +1,23 @@
+function Dificultad(){
+    window.location.href="dificultad.html";}
+
+function Estadisticas(){
+    window.location.href="estadisticas.html";
+}
+function Reglas(){
+    window.location.href="Reglas.html";}
+
+function Ingreso(){
+    window.location.href="Ingreso.html";}
+
+function NivelFacil(){
+    window.location.href="NivelFacil.html";}
+
+function NivelMedio(){
+    window.location.href="NivelMedio.html";}
+
+function NivelDificil(){
+    window.location.href="NivelDificil.html";}
+
+function NivelExtremo(){
+    window.location.href="NivelExtremo.html";}
