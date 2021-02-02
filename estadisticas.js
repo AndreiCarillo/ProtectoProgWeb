c0f45f1910c772baf67d31a97ec2817f25562294
AlanCastellanosSulub <48918200+AlanCastellanosSulub@users.noreply.github.com>
2021-02-02 18:37:56 -0500

    Add files via upload

---
+++ estadisticas.js
@@ -0,0 +1,26 @@
+function Dificultad(){
+    window.location.href="dificultad.html";}
+
+function Estadisticas(){
+    window.location.href="estadisticas.html";
+}
+function Reglas(){
+    window.location.href="Reglas.html";}
+
+function Ingreso(){
+    window.location.href="Ingreso.html";}
+function cambiarFondo1(){
+    if(document.getElementById('estilos').href.value="estadisticas"){
+        document.getElementById('estilos').href = "estadisticasVerde.css";
+    }
+}
+function cambiarFondo2(){
+    if(document.getElementById('estilos').href.value="estadisticas.css"){
+        document.getElementById('estilos').href = 'estadisticasRojo.css';
+    }
+}
+function cambiarFondo3(){
+    if(document.getElementById('estilos').href.value="estadisticas.css"){
+        document.getElementById('estilos').href = 'estadisticas.css';
+    }
+}
